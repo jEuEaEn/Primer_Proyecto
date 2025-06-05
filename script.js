78e692015a97a9b5353be8bf3a6fdea6839261f2
jEuEaEn <jarm1981avrm@gmail.com>
2025-06-05 18:17:15 -0500

    Primer cambio

---
+++ script.js
@@ -0,0 +1,10 @@
+const nombre = document.getElementById("Mane")
+const apellido = document.getElementById("surn")
+const años = document.getElementById("tick")
+const Año = document.getElementById("Año")
+
+function validar(){
+
+   alert("Datos del formulario: \nNombre: " + nombre.value +" \nApellido: "+ apellido.value +" \nAños: " + años.value + "\nAño de nacimiento" + Año.value)
+
+}
