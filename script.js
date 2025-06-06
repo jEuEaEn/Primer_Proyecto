5e9b632b5718e9db299755ce8adaa231ddf8c31b
jEuEaEn <jarm1981avrm@gmail.com>
2025-06-05 19:57:05 -0500

    Cambio radical 1

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const apellido = document.getElementById("surn")
 const años = document.getElementById("tick")
 const Año = document.getElementById("Año")
 
+const nombreResultado = document.getElementById("respuestaNombre")
+const apellidoResultado = document.getElementById("respuestaApellido")
+const anosResultado = document.getElementById("respuestaAños")
+const anoResultado = document.getElementById("respuestaAño")
+
+
 function validar(){
 
-   alert("Datos: \nNombre: " + nombre.value +" \nApellido: "+ apellido.value +" \nAños: " + años.value + "\nAño de nacimiento" + Año.value)
+   nombreResultado.innerText = " Nombre: " + nombre.value
+   apellidoResultado.innerText = " apellido: " + apellido.value
+   anosResultado.innerText = " años: " + años.value
+   anoResultado.innerText = " Año de nacimiento: " + Año.value
 
 }
