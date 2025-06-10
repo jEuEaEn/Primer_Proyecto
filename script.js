1f93c18bdf1b0cc56daf2673f10f07a2fd6845c1
jEuEaEn <jarm1981avrm@gmail.com>
2025-06-10 18:51:10 -0500

    Mayor o menor de edad

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const nombre = document.getElementById("Mane")
 const apellido = document.getElementById("surn")
 const años = document.getElementById("tick")
-const Año = document.getElementById("Año")
 
 const nombreResultado = document.getElementById("respuestaNombre")
 const apellidoResultado = document.getElementById("respuestaApellido")
 const anosResultado = document.getElementById("respuestaAños")
-const anoResultado = document.getElementById("respuestaAño")
 
 
 function validar(){
 
    nombreResultado.innerText = " Nombre: " + nombre.value
    apellidoResultado.innerText = " apellido: " + apellido.value
-   anosResultado.innerText = " años: " + años.value
-   anoResultado.innerText = " Año de nacimiento: " + Año.value
+
+   if (años.value <= 18){
+
+     anosResultado.innerText = " Estado: Menor de Edad "+ años.value
+
+   }else{
+
+     anosResultado.innerText = " Estado: Mallor de Edad y "+ años.value
+
+   }
 
 }
