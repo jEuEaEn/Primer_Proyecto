66f100ccc8e14e6c92e211ac4c88e30c231e1c93
jEuEaEn <jarm1981avrm@gmail.com>
2025-06-12 19:37:20 -0500

    Cambios en Tema

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
+/* Cuadro de arriba */
 const nombre = document.getElementById("Mane")
 const apellido = document.getElementById("surn")
 const años = document.getElementById("tick")
 
+/* Cuadro de abajo */
+const funda = document.getElementById("fondo1")
 const nombreResultado = document.getElementById("respuestaNombre")
 const apellidoResultado = document.getElementById("respuestaApellido")
 const anosResultado = document.getElementById("respuestaAños")
 
+/* Temas */
+const claro = document.getElementById("fondo2")
+const oscuro = document.getElementById("fondo3")
+
+
+function tema(){
+
+document.body.classList.toggle("dark")
+
+}
+
 
 function validar(){
 
-   nombreResultado.innerText = " Nombre: " + nombre.value
-   apellidoResultado.innerText = " apellido: " + apellido.value
+   nombreResultado.innerText = nombre.value
+   apellidoResultado.innerText = apellido.value
 
-   if (años.value <= 18){
 
-     anosResultado.innerText = " Estado: Menor de Edad "+ años.value
+
+
+   if( nombre.value.trim() == "" || apellido.value.trim() == "" ){
+    alert("Tienes que llenar las casillas")
 
    }else{
 
-     anosResultado.innerText = " Estado: Mallor de Edad y "+ años.value
+   const edadNumero = parseInt(años.value)
+   
+   if ( edadNumero <= 18){
+
+     anosResultado.innerText = " Estado: Menor de Edad con "+ años.value + " años"
+     funda.classList.add("Menor")
+     funda.classList.remove("Mayor")
+
+   }else{
+
+     anosResultado.innerText = " Estado: Mayor de Edad con "+ años.value + " años"
+     funda.classList.remove("Menor")
+     funda.classList.add("Mayor")
 
    }
+
+   }
+
 
 }
